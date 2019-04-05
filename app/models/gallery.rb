119682c42c2b87fa8e7f85811f81965bd893b20e
kreestoff <christopher.l.mcintosh@outlook.com>
2019-04-05 15:35:51 -0500

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,31 @@
 class Gallery
 
   attr_reader :name, :city
-
+  @@all = []
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.gallery == self}
+  end
+
+  def artists
+    paintings.map {|painting| painting.artist}
+  end
+
+  def artist_names
+    artists.map {|artist| artist.name}
+  end
+
+  def most_expensive_painting
+    highest_price = self.paintings.map {|painting| painting.price}.max
+    self.paintings.find {|painting| painting.price == highest_price}
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,21 @@ require 'pry'
 require_relative '../app/models/artist.rb'
 require_relative '../app/models/gallery.rb'
 require_relative '../app/models/painting.rb'
+
+artist = Artist.new("Kevin", 10)
+gallery1 = Gallery.new("Museum", "Houston")
+painting = Painting.new("Tree", 100, artist, gallery1)
+
+artist1 = Artist.new("Chris", 5)
+
+painting1 = Painting.new("bunny", 1, artist1, gallery1)
+painting2 = Painting.new("cat", 1, artist1, gallery1)
+painting3 = Painting.new("dog", 19, artist1, gallery1)
+
+artist2 = Artist.new("bob", 25)
+p1 = Painting.new("cow", 13, artist2, gallery1)
+p2 = Painting.new("raven", 15, artist2, gallery1)
+
+binding.pry
+
+0
